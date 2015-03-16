c177494a003af7624afa7cc7e5f3239886242a71
Chinmay <cpednekar@barclaycardus.com>
2015-03-15 21:19:00 -0400

    Basic master detail layout

---
+++ Foodie/SearchViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SearchViewController.swift
+//  Foodie
+//
+//  Created by Pednekar, Chinmay on 3/15/15.
+//  Copyright (c) 2015 chinmay. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("resultCell", forIndexPath: indexPath) as UITableViewCell
+        
+        cell.textLabel.text = "Row \(indexPath.row)"
+        return cell
+    }
+    
+}
